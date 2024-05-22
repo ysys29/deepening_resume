851a9079b1456c2f533a8a94431b6145d631e69b
lys <projectys29@gmail.com>
2024-05-22 13:54:43 +0900

    이력서 목록 조회 api 기초 구현

--- src/routes/resumes.router.js
+++ src/routes/resumes.router.js
@@ -6,17 +6,51 @@ const router = express.Router();
 
 router.post('/resumes', authMiddleware, async (req, res, next) => {
   const { userId } = req.user;
-  const { title, content } = req.body;
+  const { title, content, status } = req.body;
   const resume = await prisma.resumes.create({
     data: {
       UserId: userId,
       title,
       content,
+      status,
     },
   });
   return res
     .status(201)
     .json({ messge: '이력서 생성에 성공했습니다.', data: resume });
+});
+
+router.get('/resumes', authMiddleware, async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+    const resume = await prisma.resumes.findMany({
+      where: { UserId: userId },
+      select: {
+        resumeId: true,
+        title: true,
+        content: true,
+        status: true,
+        createdAt: true,
+        updatedAt: true,
+        User: {
+          select: {
+            name: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return res
+      .status(200)
+      .json({ message: '이력서 조회에 성공했습니다.', data: resume });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ errorMessage: error.message ?? '무슨 에러람' });
+  }
 });
 
 export default router;
