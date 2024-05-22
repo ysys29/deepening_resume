775f551ac9dd1bfdacc6906a8d9b7ad1aa8fbc14
lys <projectys29@gmail.com>
2024-05-22 15:25:35 +0900

    이력서 상세 조회 api 기초 구현

--- src/routes/resumes.router.js
+++ src/routes/resumes.router.js
@@ -4,6 +4,7 @@ import { prisma } from '../utils/prisma.utils.js';
 
 const router = express.Router();
 
+//이력서 생성 api
 router.post('/resumes', authMiddleware, async (req, res, next) => {
   const { userId } = req.user;
   const { title, content, status } = req.body;
@@ -20,6 +21,7 @@ router.post('/resumes', authMiddleware, async (req, res, next) => {
     .json({ messge: '이력서 생성에 성공했습니다.', data: resume });
 });
 
+//이력서 목록 조회 api
 router.get('/resumes', authMiddleware, async (req, res, next) => {
   try {
     const { userId } = req.user;
@@ -50,6 +52,40 @@ router.get('/resumes', authMiddleware, async (req, res, next) => {
     return res
       .status(400)
       .json({ errorMessage: error.message ?? '무슨 에러람' });
+  }
+});
+
+//이력서 상세 조회 api
+router.get('/resumes/:resumeId', authMiddleware, async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+    const { resumeId } = req.params;
+    const resume = await prisma.resumes.findFirst({
+      where: { UserId: userId, resumeId: +resumeId },
+      select: {
+        resumeId: true,
+        title: true,
+        content: true,
+        status: true,
+        createdAt: true,
+        updatedAt: true,
+        User: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    if (!resume) {
+      return res.status(400).json({ errorMessage: '존재하지 않는 이력서' });
+    }
+
+    return res
+      .status(200)
+      .json({ message: '이력서 상세 조회 성공', data: resume });
+  } catch (error) {
+    next(error);
   }
 });
 
