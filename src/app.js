697e9294da9c27e1dc731bb6446b08a022a4f81b
lys <projectys29@gmail.com>
2024-05-22 12:32:42 +0900

    로그인 api 기초 구현

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import UsersRouter from './routes/users.router.js';
+import cookieParser from 'cookie-parser';
 import errorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 
 const app = express();
 const PORT = 3018;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   return res.status(200).json({ message: '안ㄴ녕' });
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { prisma } from '../utils/prisma.utils.js';
-import bcrypt from 'bcrypt';
+import bcrypt, { compare } from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -25,6 +26,31 @@ router.post('/sign-up', async (req, res, next) => {
   });
 
   return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+});
+
+//로그인 api
+router.post('/sign-in', async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.users.findFirst({ where: { email } });
+  if (!user) {
+    return res.status(401).json({ errorMessage: '존재하지 않는 이메일' });
+  }
+  let decodedPassword = await bcrypt.compare(password, user.password);
+  if (!decodedPassword) {
+    return res
+      .status(401)
+      .json({ errorMessage: '비밀번호가 일치하지 않습니다.' });
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.userId,
+    },
+    'user_secret_key'
+  );
+  res.cookie('authorization', `Bearer ${token}`);
+  return res.status(200).json({ message: '로그인에 성공했습니다.' });
 });
 
 export default router;
