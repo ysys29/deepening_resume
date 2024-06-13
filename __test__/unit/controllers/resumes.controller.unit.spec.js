25c7c1dbd3874435b54beb9b233f7b2d42e260b0
lys <projectys29@gmail.com>
2024-06-13 19:00:39 +0900

    이력서 컨트롤러 테스트 코드에 유효하지 않은 이력서 아이디를 넣었을 때 에러  추가

--- __test__/unit/controllers/resumes.controller.unit.spec.js
+++ __test__/unit/controllers/resumes.controller.unit.spec.js
@@ -3,6 +3,7 @@ import { ResumesController } from '../../../src/controllers/resumes.controller.j
 import { dummyResumes } from '../../dummies/resumes.dummy.js';
 import { dummyUsers } from '../../dummies/users.dummy.js';
 import { dummyLogs } from '../../dummies/statusLog.dummy.js';
+import { HttpError } from '../../../src/errors/http.error.js';
 
 const resumesService = {
   createResume: jest.fn(),
@@ -37,6 +38,8 @@ describe('ResumesController Unit Test', () => {
     // mockResponse.status의 경우 메서드 체이닝으로 인해 반환값이 Response(자신: this)로 설정되어야합니다.
     mockResponse.status.mockReturnValue(mockResponse);
   });
+
+  const invalidResumeId = '테스트용 숫자가 아닌 이력서 아이디 값';
 
   //이력서 생성
   test('createResume Method', async () => {
@@ -120,6 +123,21 @@ describe('ResumesController Unit Test', () => {
     });
   });
 
+  //이력서 상세 조회 -ERROR
+  test('findResume Method -- 입력한 이력서 아이디가 숫자가 아니면 에러', async () => {
+    // GIVEN
+    mockRequest.user.userId = dummyUsers[1].userId;
+    mockRequest.params.resumeId = invalidResumeId;
+
+    // WHEN
+    await resumesController.findResume(mockRequest, mockResponse, mockNext);
+
+    // THEN
+    expect(mockNext).toHaveBeenCalledWith(
+      new HttpError.BadRequest('입력된 아이디의 형식이 잘못되었습니다.')
+    );
+  });
+
   // 이력서 수정
   test('editResume Method', async () => {
     // GIVEN
@@ -155,6 +173,24 @@ describe('ResumesController Unit Test', () => {
     });
   });
 
+  // 이력서 수정 -ERROR
+  test('editResume method -- 입력한 이력서 아이디가 숫자가 아니면 에러', async () => {
+    // GIVEN
+    const newTitle = '테스트용 수정 제목';
+    const newContent = '테스트용 수정 내용';
+    mockRequest.user = dummyUsers[1];
+    mockRequest.params.resumeId = invalidResumeId;
+    mockRequest.body = { title: newTitle, content: newContent };
+
+    // WHEN
+    await resumesController.editResume(mockRequest, mockResponse, mockNext);
+
+    // THEN
+    expect(mockNext).toHaveBeenCalledWith(
+      new HttpError.BadRequest('입력된 아이디의 형식이 잘못되었습니다.')
+    );
+  });
+
   //이력서 삭제
   test('deleteResume Method', async () => {
     // GIVEN
@@ -176,6 +212,21 @@ describe('ResumesController Unit Test', () => {
       message: '이력서 삭제에 성공했습니다.',
       resumeId: mockRequest.params.resumeId,
     });
+  });
+
+  //이력서 삭제 -ERROR
+  test('deleteResume Method -- 입력한 이력서 아이디가 숫자가 아니면 에러', async () => {
+    // GIVEN
+    mockRequest.user.userId = dummyUsers[1].userId;
+    mockRequest.params.resumeId = invalidResumeId;
+
+    // WHEN
+    await resumesController.deleteResume(mockRequest, mockResponse, mockNext);
+
+    // THEN
+    expect(mockNext).toHaveBeenCalledWith(
+      new HttpError.BadRequest('입력된 아이디의 형식이 잘못되었습니다.')
+    );
   });
 
   //이력서 상태 수정
@@ -213,6 +264,31 @@ describe('ResumesController Unit Test', () => {
     });
   });
 
+  //이력서 상태 수정 -ERROR
+  test('updateResumeStatus Method', async () => {
+    // GIVEN
+    const mockStatus = 'DROP';
+    const mockReason = '테스트용 이력서 상태 수정 이유';
+    mockRequest.user.userId = dummyUsers[1].userId;
+    mockRequest.params.resumeId = invalidResumeId;
+    mockRequest.body = {
+      status: mockStatus,
+      reason: mockReason,
+    };
+
+    // WHEN
+    await resumesController.updateResumeStatus(
+      mockRequest,
+      mockResponse,
+      mockNext
+    );
+
+    // THEN
+    expect(mockNext).toHaveBeenCalledWith(
+      new HttpError.BadRequest('입력된 아이디의 형식이 잘못되었습니다.')
+    );
+  });
+
   //이력서 상태 수정 로그 조회
   test('findStatusLogs Method', async () => {
     // GIVEN
@@ -238,5 +314,23 @@ describe('ResumesController Unit Test', () => {
       message: `${mockRequest.params.resumeId}번 이력서의 상태 변경 이력`,
       data: mockLogs,
     });
+  });
+
+  //이력서 상태 수정 로그 조회 -ERROR
+  test('findStatusLogs Method -- 입력한 이력서 아이디가 숫자가 아니면 에러', async () => {
+    // GIVEN
+    mockRequest.params.resumeId = invalidResumeId;
+
+    // WHEN
+    await resumesController.checkStatusLogs(
+      mockRequest,
+      mockResponse,
+      mockNext
+    );
+
+    // THEN
+    expect(mockNext).toHaveBeenCalledWith(
+      new HttpError.BadRequest('입력된 아이디의 형식이 잘못되었습니다.')
+    );
   });
 });
